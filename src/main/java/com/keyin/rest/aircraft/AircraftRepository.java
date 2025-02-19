d2214736c6a7a0612084ef8256bf652e8a28fc76
Adewale Gbadamosi <adewalegbadamosi@gmail.com>
2025-02-19 10:11:23 -0330

    Aircraft codes added

--- src/main/java/com/keyin/rest/aircraft/AircraftRepository.java
+++ src/main/java/com/keyin/rest/aircraft/AircraftRepository.java
@@ -1,4 +1,10 @@
 package com.keyin.rest.aircraft;
 
-public interface AircraftRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AircraftRepository extends CrudRepository<Aircraft, Long> {
+    public Aircraft findByAircraftName(String name);
 }
+
