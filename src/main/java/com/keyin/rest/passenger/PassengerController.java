f6c097c18f64dfeeefddabd65e01763bf36b8a9e
clovis <clovis3n@gmail.com>
2025-02-19 19:48:39 -0330

    feat: Add Passenger controller and API endpoints

--- src/main/java/com/keyin/rest/passenger/PassengerController.java
+++ src/main/java/com/keyin/rest/passenger/PassengerController.java
@@ -1,4 +1,33 @@
 package com.keyin.rest.passenger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+        import java.util.List;
+
+@RestController
+@CrossOrigin
 public class PassengerController {
+    @Autowired
+    private PassengerService passengerService;
+
+    @GetMapping("/passengers")
+    public List<Passenger> getAllPassengers() {
+        return passengerService.findAllPassengers();
+    }
+
+    @GetMapping("/passenger/{id}")
+    public Passenger getPassengerByID(@PathVariable long id) {
+        return passengerService.findPassengerById(id);
+    }
+
+    @PostMapping("/passenger")
+    public Passenger createPassenger(@RequestBody Passenger newPassenger) {
+        return passengerService.createPassenger(newPassenger);
+    }
+
+    @PutMapping("/passenger/{id}")
+    public Passenger updatePassenger(@RequestBody Passenger updatedPassenger) {
+        return passengerService.updatePassenger(updatedPassenger);
+    }
 }
