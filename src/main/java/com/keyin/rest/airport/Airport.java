4c53b751f380887a61e326e7e16fc28a3f74c22c
Adewale Gbadamosi <adewalegbadamosi@gmail.com>
2025-02-19 11:38:37 -0330

    Airport codes added

--- src/main/java/com/keyin/rest/airport/Airport.java
+++ src/main/java/com/keyin/rest/airport/Airport.java
@@ -1,4 +1,53 @@
 package com.keyin.rest.airport;
 
+import com.keyin.rest.city.City;
+import jakarta.persistence.*;
+
+@Entity
 public class Airport {
+    @Id
+    @SequenceGenerator(name = "airport_sequence", sequenceName = "airport_sequence", allocationSize = 1, initialValue=1)
+    @GeneratedValue(generator = "airport_sequence")
+    private Long id;
+
+    private String name;
+    private String code;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
 }
